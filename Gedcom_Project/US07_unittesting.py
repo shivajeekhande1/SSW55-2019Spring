d7ecd260094db2b59563dc0fdfd733e42782225a
Eshwar95 <35928597+Eshwar95@users.noreply.github.com>
2019-03-24 23:10:55 -0400

    Unittesting for Sprint 2 and test_gedcom file

---
+++ Gedcom_Project/US07_unittesting.py
@@ -0,0 +1,23 @@
+import unittest
+import Gedcom_Project
+
+class Testus07(unittest.TestCase):
+
+    def test1(self):
+        Gedcom_Project.filepath = "GedcomFiles/SampleTestFile.ged"
+        self.assertEqual(Gedcom_Project.max_age(),True)
+
+    def test2(self):
+        Gedcom_Project.filepath = "GedcomFiles/SampleTestFile.ged"
+        self.assertTrue(Gedcom_Project.max_age())
+
+    def test3(self):
+        Gedcom_Project.filepath = "GedcomFiles/SampleTestFile.ged"
+        self.assertIsNotNone(Gedcom_Project.max_age())
+
+    def test4(self):
+        Gedcom_Project.filepath= "GedcomFiles/SampleTestFile.ged"
+        self.assertNotEqual(Gedcom_Project.max_age(),False)
+
+if if __name__ == "__main__":
+    unittest.main()
